69b16de011177ac504bc9a5b5e580522716dee81
sjorgens <scottjorgens@netscape.net>
2016-01-21 14:07:07 -0600

    adds ability to send/pull skills from database

--- client/client.js
+++ client/client.js
@@ -9,17 +9,28 @@ app.controller('talentController', ['$scope', '$http', function($scope, $http){
     $scope.allTalent = [];
     $scope.skill = {};
     $scope.allSkills = [];
+    $scope.fullFormData = {};
+
 
     $scope.addTalent = function() {
-        $scope.data = $scope.formData;
-        console.log($scope.allSkills);
-        console.log(this.formData);
+        $scope.selectedSkills = [];
+        for (i = 0; i < $scope.allSkills.length; i++){
+            if ($scope.allSkills[i].checked) {
+                $scope.selectedSkills.push($scope.allSkills[i].id);
+            }
+        }
+
+        $scope.fullFormData = [$scope.formData, $scope.selectedSkills];
+        $scope.data = $scope.fullFormData;
+
+        //console.log($scope.data);
+        //
+        //console.log($scope.allSkills);
+        //console.log(this.formData);
+
         //function to POST to DB
         $http.post('/api/addTalent', $scope.data).success(function (response) {
-
             $scope.allTalent = response;
-
-
         });
     };
     $scope.getInfo = function() {
--- server/routes/api.js
+++ server/routes/api.js
@@ -11,30 +11,53 @@ router.post('/addTalent', function (request, response) {
 
     var results = [];
     var data = request.body;
+    var holder;
+    console.log(data[1][0]);
 
     pg.connect(connectionString, function (error, client) {
 
         if (error) {
             console.log(error);
         }
-        ;
 
         client.query("INSERT INTO talent(first_name, last_name, phone, low_range, high_range) VALUES($1, $2, $3, $4, $5)", [data.firstName, data.lastName, data.phone, data.salMin, data.salMax]);
 
-        var query = client.query("SELECT * FROM talent ORDER BY id ASC");
+        var talentId = client.query("SELECT id FROM talent ORDER BY id DESC LIMIT 1");
+        //console.log('talent id:', talentId);
 
-        query.on('row', function (row) {
-            results.push(row);
+        talentId.on('row', function (row) {
+            console.log('row.id:', row.id);
+            for (var i = 0; i < data[1].length; i++) {
+                console.log('data:', data[1][i]);
+                console.log(row.id);
+                client.query("INSERT INTO talent_skills(talent_id, skill_id) VALUES ($1, $2)", [row.id, data[1][i]]);
+            }
+            wrapItUp();
         });
 
-        query.on('end', function () {
-            client.end();
-            return response.json(results);
-        });
-
-    })
 
 
+        //for (var i = 0; i < data[1].length; i++) {
+        //    console.log('for:', holder);
+        //    client.query("INSERT INTO talent_skills(talent_id, skill_id) VALUES ($1, $2)", [holder, data[1][i]]);
+        //};
+        function wrapItUp() {
+
+            var query = client.query("SELECT * FROM talent JOIN talent_skills ON talent.id = talent_skills.talent_id JOIN skills ON talent_skills.skill_id = skills.id ORDER BY talent.id ASC");
+
+            query.on('row', function (row) {
+                results.push(row);
+            });
+
+
+            query.on('end', function () {
+                client.end();
+                return response.json(results);
+
+            })
+        }
+
+    });
 });
 
 router.get('/addTalent', function (request, response) {
@@ -46,7 +69,7 @@ router.get('/addTalent', function (request, response) {
         if (error) {
             console.log(error);
         }
-        ;
+
 
         var query = client.query("SELECT * FROM talent ORDER BY id ASC");
 
